8d4aaa0c04694cae68c445f8a093113dd49abcce
Hans Indino <sipher_68@yahoo.com>
2023-02-13 23:08:18 +0800

    Fixed next-auth/react Session Wrapping error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import { Inter } from '@next/font/google';
+import { signIn, useSession } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <div>Hello Home</div>;
+  const { data } = useSession();
+
+  console.log('Here is Data', data);
+
+  return (
+    <div>
+      <button onClick={() => signIn('google')}>Sign In</button>
+    </div>
+  );
 }
