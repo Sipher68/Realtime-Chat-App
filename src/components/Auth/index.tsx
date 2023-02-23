5e1f256f8a6b39d6c02dc148547dce04138f8694
Hans Indino <sipher_68@yahoo.com>
2023-02-23 13:39:33 +0800

    Added user operations for user mutations

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,7 +1,9 @@
+import { useMutation } from '@apollo/client';
 import { Button, Center, Stack, Text, Image, Input } from '@chakra-ui/react';
 import { Session } from 'next-auth';
 import { signIn } from 'next-auth/react';
 import { useState } from 'react';
+import UserOperations from '../../graphql/operations/user';
 
 interface IAuthProps {
   session: Session | null;
@@ -14,9 +16,13 @@ const Auth: React.FunctionComponent<IAuthProps> = ({
 }) => {
   const [username, setUsername] = useState('');
 
+  const [createUsername, { data, loading, error }] = useMutation(
+    UserOperations.Mutations.createUsername
+  );
+
   const onSubmit = async () => {
     try {
-      // create username mutation to send username to GraphQL API
+      await createUsername({ variables: { username } });
     } catch (error) {
       console.log('onSubmit error', error);
     }
